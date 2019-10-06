cee0a291e7bdc5fd9fddd19ebd100158d46e6582
Hitoshi Harada <hitoshi@alpaca.markets>
2019-10-05 19:37:04 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,138 @@
+# Concurrent Scalp Algo
+
+This python script is a working example to execute scalping trading algorithm for
+[Alpaca API](https://alpaca.markets). This algorithm uses real time order updates
+as well as minute level bar streaming from Polygon via Websockets (see
+[document](https://docs.alpaca.markets/market-data/#consolidated-market-data) for
+Polygon data access).
+One of the contributions of this example is to demonstrate how to handle
+multiple stocks concurrently as independent routine using Python's asyncio.
+
+The strategy holds positions for very short period and exits positions quickly, so
+you have to have more than $25k equity in your account due to the Pattern Day Trader rule,
+to run this example. For more information about PDT rule, please read the
+[document](https://docs.alpaca.markets/user-protections/#the-rule).
+
+## Dependency
+This script needs latest [Alpaca Python SDK](https://github.com/alpacahq/alpaca-trade-api-python).
+Please install it using pip
+
+```sh
+$ pip3 install alpaca-trade-api
+```
+
+or use pipenv using `Pipfile` in this directory.
+
+```sh
+$ pipenv install
+```
+
+## Usage
+
+```sh
+$ python main.py --lot=2000 TSLA FB AAPL
+```
+
+You can specify as many symbols as you want.  The script is designd to kick off while market
+is open. Nothing would happen until 21 minutes from the market open as it relies on the
+simple moving average as the buy signal.
+
+
+## Strategy
+The algorithm idea is to buy the stock upon the buy signal (MA20/price cross over in 1-minute bar) as
+much as `lot` amount of dollar, then immediately sell the position at or above the entry price.
+The buy signal is expremely simple, but what this strategy achieves is the quick reaction to
+exit the position as soon as the buy order fills. There are reasonable chances that you can sell
+the positions at the better prices than your entry within the short period of time. We send
+limit order at the last trade or entry price whichever the higher to avoid unnecessary slippage.
+
+The buy order is canceled after 2 minutes if it does not fill, assuming the signal is not
+effective anymore. This could happen in a fast-moving market situation. Sells are left
+indifinitely until it fills, but this may cause loss more than the accumulated profit depending
+on the market situation. This is where you can improve the risk control beyond this example.
+
+The buy signal is calculated as soon as a minute bar arrives, which typically happen about 4 seconds
+after the top of every minute (this is Polygon's behavior for minute bar streaming).
+
+This example liquidates all watching positions with market order at the end of market hours (03:55pm ET).
+
+
+## Implementation
+This example heavily relies on Python's asyncio. Although the thread is single, we handle
+multiple symbols concurrently using this async loop.
+
+We keep track of each symbol state in a separate `ScalpAlgo` class instance. That way,
+everything stays simple without complex data struture and easy to read. The `main()`
+function creates the algo instance for each symbol and creates streaming object
+to listen the bar events. As soon as we receive a minute bar, we invoke event handler
+for each symbol.
+
+The main routine also starts a period check routine to do some work in background every 30 seconds.
+In this background task, we check market state with the clock API and liquidate positions
+before the market closes.
+
+### Algo Instance and State Management
+Each algo instance initializes its state by fetching day's bar data so far and position/order
+from Alpaca API to synchronize, in case the script restarts after some trades. There are
+four internal states and transitions as events happen.
+
+- `TO_BUY`: no position, no order. Can transition to `BUY_SUBMITTED`
+- `BUY_SUBMITTED`: buy order has been submitted. Can transition to `TO_BUY` or `TO_SELL`
+- `TO_SELL`: buy is filled and holding position. Can transition to `SELL_SUBMITTED`
+- `SELL_SUBMITTED`: sell order has been submitted. Can transition to `TO_SELL` or `TO_BUY`
+
+### Event Handlers
+`on_bar()` is an event handler for the bar data. Here we calculate signal that triggers
+a buy order in the `TO_BUY` state. Once order is submitted, it goes to the `BUY_SUBMITTED`
+state.
+
+If order is filled, `on_order_update()` handler is called with `event=fill`. The state
+transitions to `TO_SELL` and immediately submits a sell order, to transition to the
+`SELL_SUBMITTED` state.
+
+Orders may be canceled or rejected (caused by this script or you manually cancel them
+from the dashboard). In these cases, the state transitions to `TO_BUY` (if not holding
+a position) or `TO_SELL` (if holding a position) and wait for the next events.
+
+`checkup()` method is the background periodic job to check several conditions, where
+we cancel open orders and sends market sell order if there is an open position.
+
+It exits once the market closes.
+
+### Note
+Each algo instance owns its child logger, prefixed by the symbol name. The console
+log is also emitted to a file `console.log` under the same directory for your later review.
+
+Again, the beautify of this code is that there is no multithread code but each
+algo instance can focus on the bar/order/position data only for its own. It still
+handles multiple symbols concurrently plus runs background periodic job in the
+same async loop.
+
+The trick to run additional async routine is as follows.
+
+```py
+    loop = stream.loop
+    loop.run_until_complete(asyncio.gather(
+        stream.subscribe(channels),
+        periodic(),
+    ))
+    loop.close()
+```
+
+We use `asyncio.gather()` to run all bar handler, order update handler and periodic job
+in one async loop indifinitely. You can kill it by `Ctrl+C`.
+
+### Customization
+Instead of using this buy signal of 20 minute simple moving average cross over, you can
+use your own buy signal. To do so, extend the `ScalpAlgo` class and write your own
+`_calc_buy_signal()` method.
+
+```py
+    class MyScalpAlgo(ScalpAlgo):
+        def _calculate_buy_signal(self):
+            '''self._bars has all minute bars in the session so far. Return True to
+            trigger buy order'''
+            pass
+```
+
+And use it instead of the original class.
---
+++ main.py
@@ -0,0 +1,262 @@
+import alpaca_trade_api as alpaca
+import asyncio
+import pandas as pd
+import sys
+
+import logging
+
+logger = logging.getLogger()
+
+
+class ScalpAlgo:
+
+    def __init__(self, api, symbol, lot):
+        self._api = api
+        self._symbol = symbol
+        self._lot = lot
+        self._bars = []
+        self._l = logger.getChild(self._symbol)
+
+        now = pd.Timestamp.now(tz='America/New_York').floor('1min')
+        market_open = now.replace(hour=9, minute=30)
+        today = now.strftime('%Y-%m-%d')
+        tomorrow = (now + pd.Timedelta('1day')).strftime('%Y-%m-%d')
+        data = api.polygon.historic_agg_v2(
+            symbol, 1, 'minute', today, tomorrow, unadjusted=False).df
+        bars = data[market_open:]
+        self._bars = bars
+
+        self._init_state()
+
+    def _init_state(self):
+        symbol = self._symbol
+        order = [o for o in self._api.list_orders() if o.symbol == symbol]
+        position = [p for p in self._api.list_positions()
+                    if p.symbol == symbol]
+        self._order = order[0] if len(order) > 0 else None
+        self._position = position[0] if len(position) > 0 else None
+        if self._position is not None:
+            if self._order is None:
+                self._state = 'TO_SELL'
+            else:
+                self._state = 'SELL_SUBMITTED'
+                if self._order.side != 'sell':
+                    self._l.warn(
+                        f'state {self._state} mismatch order {self._order}')
+        else:
+            if self._order is None:
+                self._state = 'TO_BUY'
+            else:
+                self._state = 'BUY_SUBMITTED'
+                if self._order.side != 'buy':
+                    self._l.warn(
+                        f'state {self._state} mismatch order {self._order}')
+
+    def _now(self):
+        return pd.Timestamp.now(tz='America/New_York')
+
+    def _outofmarket(self):
+        return self._now().time() >= pd.Timestamp('15:55').time()
+
+    def checkup(self, position):
+        # self._l.info('periodic task')
+
+        now = self._now()
+        order = self._order
+        if (order is not None and
+            order.side == 'buy' and now -
+                order.submitted_at > pd.Timedelta('2 min')):
+            last_price = self._api.polygon.last_trade(self._symbol).price
+            self._l.info(
+                f'canceling missed buy order {order.id} at {order.limit_price} '
+                f'(current price = {last_price})')
+            self._cancel_order()
+
+        if self._position is not None and self._outofmarket():
+            self._submit_sell(bailout=True)
+
+    def _cancel_order(self):
+        if self._order is not None:
+            self._api.cancel_order(self._order.id)
+
+    def _calc_buy_signal(self):
+        mavg = self._bars.rolling(20).mean().close.values
+        closes = self._bars.close.values
+        if closes[-2] < mavg[-2] and closes[-1] > mavg[-1]:
+            self._l.info(
+                f'buy signal: closes[-2] {closes[-2]} < mavg[-2] {mavg[-2]} '
+                f'closes[-1] {closes[-1]} > mavg[-1] {mavg[-1]}')
+            return True
+        else:
+            self._l.info(
+                f'closes[-2:] = {closes[-2:]}, mavg[-2:] = {mavg[-2:]}')
+            return False
+
+    def on_bar(self, bar):
+        self._bars = self._bars.append(pd.DataFrame({
+            'open': bar.open,
+            'high': bar.high,
+            'low': bar.low,
+            'close': bar.close,
+            'volume': bar.volume,
+        }, index=[bar.start]))
+
+        self._l.info(
+            f'received bar start = {bar.start}, close = {bar.close}, len(bars) = {len(self._bars)}')
+        if len(self._bars) < 21:
+            return
+        if self._outofmarket():
+            return
+        if self._state == 'TO_BUY':
+            signal = self._calc_buy_signal()
+            if signal:
+                self._submit_buy()
+
+    def on_order_update(self, event, order):
+        self._l.info(f'order update: {event} = {order}')
+        if event == 'fill':
+            self._order = None
+            if self._state == 'BUY_SUBMITTED':
+                self._position = self._api.get_position(self._symbol)
+                self._transition('TO_SELL')
+                self._submit_sell()
+                return
+            elif self._state == 'SELL_SUBMITTED':
+                self._position = None
+                self._transition('TO_BUY')
+                return
+        elif event == 'partial_fill':
+            self._position = self._api.get_position(self._symbol)
+            self._order = self._api.get_order(order['id'])
+            return
+        elif event in ('canceled', 'rejected'):
+            if event == 'rejected':
+                self._l.warn(f'order rejected: current order = {self._order}')
+            self._order = None
+            if self._state == 'BUY_SUBMITTED':
+                if self._position is not None:
+                    self._transition('TO_SELL')
+                    self._submit_sell()
+                else:
+                    self._transition('TO_BUY')
+            elif self._state == 'SELL_SUBMITTED':
+                self._transition('TO_SELL')
+                self._submit_sell(bailout=True)
+            else:
+                self._l.warn(f'unexpected state for {event}: {self._state}')
+
+    def _submit_buy(self):
+        trade = self._api.polygon.last_trade(self._symbol)
+        amount = int(self._lot / trade.price)
+        try:
+            order = self._api.submit_order(
+                symbol=self._symbol,
+                side='buy',
+                type='limit',
+                qty=amount,
+                time_in_force='day',
+                limit_price=trade.price,
+            )
+        except Exception as e:
+            self._l.info(e)
+            self._transition('TO_BUY')
+            return
+
+        self._order = order
+        self._l.info(f'submitted buy {order}')
+        self._transition('BUY_SUBMITTED')
+
+    def _submit_sell(self, bailout=False):
+        params = dict(
+            symbol=self._symbol,
+            side='sell',
+            qty=self._position.qty,
+            time_in_force='day',
+        )
+        if bailout:
+            params['type'] = 'market'
+        else:
+            current_price = float(
+                self._api.polygon.last_trade(
+                    self._symbol).price)
+            cost_basis = float(self._position.avg_entry_price)
+            limit_price = max(cost_basis + 0.01, current_price)
+            params.update(dict(
+                type='limit',
+                limit_price=limit_price,
+            ))
+        try:
+            order = self._api.submit_order(**params)
+        except Exception as e:
+            self._l.error(e)
+            self._transition('TO_SELL')
+            return
+
+        self._order = order
+        self._l.info(f'submitted sell {order}')
+        self._transition('SELL_SUBMITTED')
+
+    def _transition(self, new_state):
+        self._l.info(f'transition from {self._state} to {new_state}')
+        self._state = new_state
+
+
+def main(args):
+    api = alpaca.REST()
+    stream = alpaca.StreamConn()
+
+    fleet = {}
+    symbols = args.symbols
+    for symbol in symbols:
+        algo = ScalpAlgo(api, symbol, lot=args.lot)
+        fleet[symbol] = algo
+
+    @stream.on(r'^AM')
+    async def on_bars(conn, channel, data):
+        if data.symbol in fleet:
+            fleet[data.symbol].on_bar(data)
+
+    @stream.on(r'trade_updates')
+    async def on_trade_updates(conn, channel, data):
+        logger.info(f'trade_updates {data}')
+        symbol = data.order['symbol']
+        if symbol in fleet:
+            fleet[symbol].on_order_update(data.event, data.order)
+
+    async def periodic():
+        while True:
+            if not api.get_clock().is_open:
+                logger.info('exit as market is not open')
+                sys.exit(0)
+            await asyncio.sleep(30)
+            positions = api.list_positions()
+            for symbol, algo in fleet.items():
+                pos = [p for p in positions if p.symbol == symbol]
+                algo.checkup(pos[0] if len(pos) > 0 else None)
+    channels = ['trade_updates'] + [
+        'AM.' + symbol for symbol in symbols
+    ]
+
+    loop = stream.loop
+    loop.run_until_complete(asyncio.gather(
+        stream.subscribe(channels),
+        periodic(),
+    ))
+    loop.close()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    fmt = '%(asctime)s:%(filename)s:%(lineno)d:%(levelname)s:%(name)s:%(message)s'
+    logging.basicConfig(level=logging.INFO, format=fmt)
+    fh = logging.FileHandler('console.log')
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(logging.Formatter(fmt))
+    logger.addHandler(fh)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('symbols', nargs='+')
+    parser.add_argument('--lot', type=float, default=2000)
+
+    main(parser.parse_args())
